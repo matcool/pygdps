c03e2f274e284d60db7740ce9461fe2965923d0b
matcool <26722564+matcool@users.noreply.github.com>
2020-01-23 22:19:24 -0300

    remove old code

--- hashes.py
+++ hashes.py
@@ -16,14 +16,6 @@ def hash_levels(levels) -> str:
     data = ''.join(f"{str(lvl['id'])[0]}{str(lvl['id'])[-1]}{lvl['stars']}0" for lvl in levels)
     return hashlib.sha1(bytes(data + 'xI25fpAapCQg', 'utf-8')).hexdigest()
 
-    levels_id = levels_id.split(',')
-    final = ''
-    for level in levels:
-        lvl = LEVELS[int(i)-1]
-        # what the fuck
-        final += f"{str(i)[0]}{str(i)[-1]}{lvl['stars']}0"
-    return hashlib.sha1(bytes(final + 'xI25fpAapCQg', 'utf-8')).hexdigest()
-
 def hash_mappack(mappacks) -> str:
     """
     Used in:
@@ -32,11 +24,3 @@ def hash_mappack(mappacks) -> str:
     # '{first digit of mappack id}{last digit of mappack id}{coins}'
     data = ''.join(f"{str(mp['id'])[0]}{str(mp['id'])[-1]}{mp['coins']}" for mp in mappacks)
     return hashlib.sha1(bytes(data + 'xI25fpAapCQg', 'utf-8')).hexdigest()
-
-    mappack_ids = mappack_ids.split(',')
-    final = ''
-    for i in mappack_ids:
-        mp = MAPPACKS[int(i)]
-        # what the fuck
-        final += f"{str(i)[0]}{str(i)[-1]}{mp['stars']}{mp['coins']}"
-    return hashlib.sha1(bytes(final + 'xI25fpAapCQg', 'utf-8')).hexdigest()
