addd7037483aee3462f5cc6ea7faff39f1767074
matcool <26722564+matcool@users.noreply.github.com>
2020-05-12 12:47:38 -0300

    Improvements to XOR functions

--- xor.py
+++ xor.py
@@ -1,4 +1,5 @@
 from base64 import b64encode, b64decode
+import itertools
 
 class InvalidXORKey(Exception): pass
 
@@ -7,13 +8,15 @@ GJP_KEY = '37526'
 def decode(string: str, key: str) -> str:
     if not key.isnumeric():
         raise InvalidXORKey('Invalid XOR key')
-    return ''.join(
-        chr(c ^ ord(key[i % len(key)])) for i, c in enumerate(b64decode(string))
-    )
+    key = key.encode('ascii')
+    return bytes(
+        c ^ k for c, k in zip(b64decode(string), itertools.cycle(key))
+    ).decode()
 
 def encode(string: str, key: str) -> str:
     if not key.isnumeric():
         raise InvalidXORKey('Invalid XOR key')
+    key = key.encode('ascii')
     return b64encode(bytes(
-        ord(c) ^ ord(key[i % len(key)]) for i, c in enumerate(string)
+        ord(c) ^ k for c, k in zip(string, itertools.cycle(key))
     )).decode()
