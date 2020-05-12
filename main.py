747a0371a77b6e90435e02f4b86dae2e94ee37f8
matcool <26722564+matcool@users.noreply.github.com>
2020-05-12 12:48:16 -0300

    Add colors to output

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import os
 from importlib import import_module
 from context import Context
+from colorama import init, Fore
 
 app = Flask(__name__)
 
@@ -12,16 +13,17 @@ def root():
 
 @app.errorhandler(404)
 def err(e):
-    print(f'Unhandled request! {request.path} {json.dumps(request.values.to_dict())}')
+    print(f'{Fore.YELLOW}Unhandled request! {Fore.LIGHTWHITE_EX}{request.path} {Fore.LIGHTBLACK_EX}{json.dumps(request.values.to_dict())}{Fore.RESET}')
     return '-1'
 
 ctx = Context
 ctx.app = app
+init() # init colorama
 
 for root, _, files in os.walk('routes'):
     for file in files:
         if not file.endswith('.py'): continue
         # this is very hacky but whatever
         path = os.path.join(root, file[:-3]).replace(os.sep, '.')
-        print(f'Importing module: {path}')
+        print(f'{Fore.LIGHTBLACK_EX}Importing module: {Fore.RESET}{path}')
         import_module(path).setup(ctx)
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+from colorama import Fore
 
 os.environ['FLASK_APP'] = 'main.py'
 os.environ['FLASK_ENV'] = 'development'
@@ -6,4 +7,4 @@ os.environ['FLASK_ENV'] = 'development'
 try:
     os.system('flask run')
 except KeyboardInterrupt:
-    pass
+    print(f'{Fore.RED}CTRL+C pressed, exiting{Fore.RESET}')
